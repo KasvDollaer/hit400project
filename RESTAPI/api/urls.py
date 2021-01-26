66b87f48aba748492e0b41fb07de33952abd325a
KasvDollaer <zvikokasvosve@gmail.com>
2021-01-26 14:00:30 +0200

    --- Auto-Git Commit ---

--- RESTAPI/api/urls.py
+++ RESTAPI/api/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('', homepage, name="Home")
-    # path('/faults', faults_lists, name="Faults_lists")
-    # path('/incidents', incidents, name="incidents")
-    # path('/Loadshedding', Loadshedding, name="Loadshedding")
+    # path('', ListPage, name="Home"),
     # path('/faults/details', fault_details, name="fault_details")
     # path('/incidents/details', incidents_details, name="incidents_details")
     # # path('', homepage, name="Home")
