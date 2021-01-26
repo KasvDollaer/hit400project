018fe4b55ecfa1a6df448a7210cfacaaecc8419e
KasvDollaer <zvikokasvosve@gmail.com>
2021-01-26 16:31:03 +0200

    --- Auto-Git Commit ---

--- RESTAPI/api/urls.py
+++ RESTAPI/api/urls.py
@@ -5,7 +5,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('', views.ListPage, name="Home"),
-    # path('/faults/details', fault_details, name="fault_details")
+    path('faults/<int:faultnumber>/', views.FaultDetails, name="fault_details")
     # path('/incidents/details', incidents_details, name="incidents_details")
     # # path('', homepage, name="Home")
     # path('', homepage, name="Home")
