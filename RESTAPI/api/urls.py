392849ae3e686116b84c08e1d6e166fd2d718d33
KasvDollaer <zvikokasvosve@gmail.com>
2021-03-03 11:30:22 +0200

    --- Auto-Git Commit ---

--- RESTAPI/api/urls.py
+++ RESTAPI/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = 'api'
 
 urlpatterns = [
     path('', views.ListPage, name="Home"),
-    path('<int:Fault_number>/', views.FaultDetails, name="fault_details")
+    path('<int:Fault_number>/', views.FaultDetails, name="fault_details"),
+    #api urls
+    path('api/', FaultAPIView.as_view()),
     # path('/incidents/details', incidents_details, name="incidents_details")
     # # path('', homepage, name="Home")
     # path('', homepage, name="Home")
